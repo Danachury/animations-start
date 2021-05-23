ebbf28782c18d28f31f80f5aac4f76023ce2c017
Denilson Anachury <denilsoncastro1997@gmail.com>
2021-05-23 13:01:39 -0500

    Inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,136 @@
+import { Component } from '@angular/core';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0px) scale(.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            opacity: 0,
+            offset: 0,
+            transform: 'translateX(-100px)'
+          }),
+          style({
+            opacity: .5,
+            offset: .3,
+            transform: 'translateX(-50px)'
+          }),
+          style({
+            opacity: 1,
+            offset: .8,
+            transform: 'translateX(-20px)'
+          }),
+          style({
+            opacity: 1,
+            offset: 1,
+            transform: 'translateX(0px)'
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(800, style({
+            opacity: 0,
+            transform: 'translateX(100px)'
+          }))
+        ])
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onAnimate() {
+    this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onDelete(item: string) {
+    this.list = this.list.filter(it => it !== item);
+  }
+
+  animationStarted($event: any) {
+    console.log($event);
+  }
+
+  animationEnded($event: any) {
+    console.log($event);
+  }
+}
